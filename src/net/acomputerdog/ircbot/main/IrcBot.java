ddf37a44e7f9a26fca0208648ba24ff09a269411
warriordog <acomputerdog@gmail.com>
2015-01-04 10:22:04 -0500

    increment version

--- src/net/acomputerdog/ircbot/main/IrcBot.java
+++ src/net/acomputerdog/ircbot/main/IrcBot.java
@@ -143,7 +143,7 @@ public class IrcBot {
     }
 
     public String getVersionString() {
-        return "AcomputerBot v0.4";
+        return "AcomputerBot v0.5";
     }
 
     public boolean canRun() {
